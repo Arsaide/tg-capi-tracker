27413fe9a301596843e65567ffa163b2431bdf97
kirill_mur <kiril.murahovskiy@gmail.com>
2026-06-01 03:28:14 +0300

    add trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { json } from 'express';
 import { AppModule } from './app.module';
+import {NestExpressApplication} from "@nestjs/platform-express";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        logger:
+            process.env.NODE_ENV === 'development'
+                ? ['log', 'error', 'warn', 'debug', 'verbose']
+                : ['error', 'warn']
+    })
+
+    app.set('trust proxy', 1)
     app.use(json());
 
     app.enableCors({ origin: true });
